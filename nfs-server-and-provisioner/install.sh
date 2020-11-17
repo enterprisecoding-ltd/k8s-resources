2ef5cf585c74cf6974d890b620a36892da9554f8
Fatih Boy <fatih@enterprisecoding.com>
2020-11-17 18:18:17 +0300

    Storage class and install script added for nfs server

---
+++ nfs-server-and-provisioner/install.sh
@@ -0,0 +1,8 @@
+NAMESPACE=nfs-server
+BASE_URL=https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-resources/main/nfs-server-and-provisioner
+
+kubectl create ns $NAMESPACE
+kubectl create -f $BASE_URL/psp.yaml 
+curl $BASE_URL/rbac.yaml | sed -E "s/namespace:.*/namespace: $NAMESPACE/g" | kubectl create -f -
+kubectl create -f $BASE_URL/deployment.yaml -n $NAMESPACE
+kubectl create -f $BASE_URL/storageclass.yaml
