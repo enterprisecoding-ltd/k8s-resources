b357c8f798f0cf708f281196342daba559a2e302
Fatih Boy <fatih@enterprisecoding.com>
2020-11-17 18:22:11 +0300

    uninstall script added for nfs-server

---
+++ nfs-server-and-provisioner/uninstall.sh
@@ -0,0 +1,9 @@
+NAMESPACE=nfs-server
+BASE_URL=https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-resources/main/nfs-server-and-provisioner
+
+kubectl delete -f $BASE_URL/storageclass.yaml
+kubectl delete -f $BASE_URL/deployment.yaml -n $NAMESPACE
+curl $BASE_URL/rbac.yaml | sed -E "s/namespace:.*/namespace: $NAMESPACE/g" | kubectl delete -f -
+kubectl delete -f $BASE_URL/psp.yaml 
+kubectl delete ns $NAMESPACE
+
