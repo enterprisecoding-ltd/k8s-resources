4d79be8aaded6df18b390b7e274844c528ee2ebf
Fatih Boy <fatih@enterprisecoding.com>
2020-11-22 21:54:14 +0300

    install master fix

--- k8s-installation-2node/install-master.sh
+++ k8s-installation-2node/install-master.sh
@@ -87,7 +87,7 @@ EOF
 
 JOIN_COMMAND=$(kubeadm token create --print-join-command)
 
-ssh ${WORKER_NODE_USER}@${WORKER_NODE_IP} https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-resources/main/k8s-installation-2node/install-worker.sh | sh -
+ssh ${WORKER_NODE_USER}@${WORKER_NODE_IP} curl -sfL https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-resources/main/k8s-installation-2node/install-worker.sh | sh -
 ssh ${WORKER_NODE_USER}@${WORKER_NODE_IP} $JOIN_COMMAND
 
 echo "Waiting worker node to be ready"
